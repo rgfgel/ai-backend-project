4864b24f69b102c63558c3668ea6e7ff9f834ca5
Rafael Ribeiro <rgfgel@gmail.com>
2026-04-09 17:23:37 -0300

    feat: introduce application layer with getStatus use case

--- src/interfaces/routes.js
+++ src/interfaces/routes.js
@@ -1,3 +1,19 @@
+const getStatus = require("../application/getStatus");
+
+module.exports = {
+  "/": (req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("API funcionando");
+  },
+
+  "/status": (req, res) => {
+    const result = getStatus();
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(result));
+  },
+};
+/*
 module.exports = {
   "/": (req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
@@ -9,3 +25,4 @@ module.exports = {
     res.end(JSON.stringify({ status: "ok" }));
   },
 };
+*/
